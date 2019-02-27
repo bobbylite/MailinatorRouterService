6d36bf0348a5115faf39a06e45ab31a25fc3a9ca
Robert Luisi <bobby.luisi@gmail.com>
2019-02-27 01:34:25 -0500

    remove gitignore files

--- build/Infrastructure/DependencyInjection/Types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Types = {
-    IApplicationResolver: Symbol("IApplicationResolver"),
-    IService: Symbol("IService"),
-    IFileWatcher: Symbol("IFileWatcher")
-};
-exports.default = Types;
