1ec143a77faaef26fd706d04bd9371fe5312c69a
Robert Luisi <bobby.luisi@gmail.com>
2019-02-26 23:34:32 -0500

    Implemented FileReader in ExcelReaderService.

--- App/Application/Services/ExcelReaderService.ts
+++ App/Application/Services/ExcelReaderService.ts
@@ -8,7 +8,7 @@ export class ExcelReaderService implements IExcelReaderService {
     public async Read(file: string): Promise<void> {
         try {
             var WorkBook: any = XLSX.readFile(file);
-            var FirstWorkSheet: any = WorkBook.Sheets[WorkBook.SheetNames[0]];
+            var FirstWorkSheet: object = WorkBook.Sheets[WorkBook.SheetNames[0]];
             var WorkSheetJson: object = XLSX.utils.sheet_to_json(FirstWorkSheet);
             
             this.ParseWorkSheet(WorkSheetJson);
