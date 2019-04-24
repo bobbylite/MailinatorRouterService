c29e84a48a1da5ab74f380bac5ea81b400cd93bd
bobby3501 <bobby.luisi@gmail.com>
2019-04-23 20:58:39 -0400

    Made NodeMailer Service Send async.

--- App/Application/Services/NodeMailerService.ts
+++ App/Application/Services/NodeMailerService.ts
@@ -8,28 +8,42 @@ import { SubjectIdentifier } from "../../Model/SubjectIdentifier";
 @injectable()
 export class NodeMailerService implements INodeMailerService {
 
-    public Send(content: any): void {
-        console.log(content);
-        let transporter = nodemailer.createTransport({
-            service: "Gmail",
-            auth: {
-                user: SendingEmail,
-                pass: SendingEmailPassword
-            }
-        });
+    public async Send(content: any) : Promise<void> {
+        await this.SendAsync(content);
+    }
 
-        let mailOptions = {
-            from: content.from,
-            to: DestinationEmail,
-            subject: content.inbox + " Mailinator Inbox",
-            html: content.html
-        };
+    private SendAsync(content: any) : Promise<boolean> {
+        return new Promise<boolean>((resolve, reject)=> {
+            try {
+                console.log(content);
+                let transporter = nodemailer.createTransport({
+                    service: "Gmail",
+                    auth: {
+                        user: SendingEmail,
+                        pass: SendingEmailPassword
+                    }
+                });
+        
+                let mailOptions = {
+                    from: content.from,
+                    to: DestinationEmail,
+                    subject: content.inbox + " Mailinator Inbox",
+                    html: content.html
+                };
+        
+                transporter.sendMail(mailOptions, (err: any, info: any) => {
+                    if (err) {
+                        reject(false);
+                        return console.log(err);
+                    }
 
-        transporter.sendMail(mailOptions, (err: any, info: any) => {
-            if (err) {
-                return console.log(err);
+                    console.log('Message sent: %s', info.messageId);
+                    resolve(true);
+                });
+            } catch(err) {
+                console.log("Error in SendAsync()");
+                console.log(err);
             }
-            console.log('Message sent: %s', info.messageId);
         });
     }
 }
